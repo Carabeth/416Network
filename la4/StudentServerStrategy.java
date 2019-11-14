c82bc88b445e6fee655792eb04aa16f7e014d5bc
tjb101825 <tjb3@email.sc.edu>
2019-11-13 23:26:16 -0500

    Add files via upload

--- la4/StudentServerStrategy.java
+++ la4/StudentServerStrategy.java
@@ -2,7 +2,14 @@ import java.util.*;
 
 public class StudentServerStrategy implements ServerStrategy{
     List<String> file;
-
+	boolean[] acks;
+	boolean slowStart;
+	boolean congestionAvoidance;
+	boolean timeout;
+	boolean fastRetransmit;
+	int cwnd;
+	int ssthresh;
+	
 
     public StudentServerStrategy(){
         reset();
@@ -13,15 +20,40 @@ public class StudentServerStrategy implements ServerStrategy{
     }
 
     public void reset(){
-
+		if (fastRetransmit == true) {
+			cwnd = cwnd/2;
+			ssthresh = ssthresh/2;
+			slowStart = true;
+			congestionAvoidance = false;
+			fastRetransmit = false;
+			timeout = false;
+		}
+		else if (timeout == true) {
+			cwnd = 1;
+			ssthresh = ssthresh/2;
+			slowStart = true;
+			congestionAvoidance = false;
+			fastRetransmit = false;
+			timeout = false;
+		}
 
     }
 
     public List<Message> sendRcv(List<Message> clientMsgs){
-        
-        return clientMsgs; //obviously wrong
-
-
+		for (Message m: clientMsgs) {
+			acks[m.num-1] = true;
+			System.out.println(m.num+","+m.msg);
+		}
+		int firstUnACKed = 0;
+		
+		List<Message> msgs = new ArrayList<Message>();
+		
+		while (firstUnACKed < acks.length && acks[firstUnACKed]) ++firstUnACKed;
+		
+		if(firstUnACKed < acks.length) {
+			msgs.add(new Message(firstUnACKed,file.get(firstUnACKed)));
+		}
+		return msgs;
     }
     
 }
